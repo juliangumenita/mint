ecb1017e1e8b7872250f942841cddda82642cc02
Julian Gumenita <julian@gumenita.com>
2018-11-19 02:12:32 +0300

    Create class.Random.php
    Added Random class fro generating numbers and strings with different set of options.

---
+++ class/class.Random.php
@@ -0,0 +1,32 @@
+<?php
+  class Random{
+    public static function number(int $_MIN, int $_MAX, $BETWEEN = FALSE){
+      $MIN = ($BETWEEN) ? $_MIN + 1 : $_MIN;
+      $MAX = ($BETWEEN) ? $_MAX - 1 : $_MAX;
+      return rand($MIN, $MAX);
+    }
+    public static function string($LENGHT = 16, $MODE = "all"){
+      $SET = array(
+        "lovercase" => "abcdefghijklmnopqrstuvwxyz",
+        "uppercase" => "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "number" => "0123456789"
+      );
+      $LIST = array(
+        "all" => $SET["lovercase"] . $SET["uppercase"] . $SET["number"],
+        "number" => $SET["number"],
+        "lovercase" => $SET["lovercase"],
+        "lovercase-number" => $SET["lovercase"] . $SET["number"],
+        "uppercase" => $SET["uppercase"],
+        "uppercase-number" => $SET["uppercase"] . $SET["number"],
+        "lovercase-uppercase" => $SET["lovercase"] . $SET["uppercase"]
+      );
+      $CHARSET = $LIST[$MODE];
+      $CHARSET_LENGHT = strlen($CHARSET);
+      $STRING = NULL;
+      for ($i = 0; $i < $LENGHT; $i++) {
+          $STRING .= $CHARSET[rand(0, $CHARSET_LENGHT - 1)];
+      }
+      return $STRING;
+    }
+  }
+?>
